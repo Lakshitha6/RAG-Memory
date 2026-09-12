fb4a3018ff17bb2ab52dab9fce1b476965ebc5d3
Lakshitha 69 <148607270+Lakshitha6@users.noreply.github.com>
2026-09-12 09:24:44 +0530

    changed base url to deployed url

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,3 +1,8 @@
+// const CONFIG = {
+//     API_BASE: 'http://localhost:8000'  // swap for local dev
+//   };
+
 const CONFIG = {
-    API_BASE: 'http://localhost:8000'  // swap for local dev
+    API_BASE: 'https://rag-memory-p0qu.onrender.com'
   };
+
